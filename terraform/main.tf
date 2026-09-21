9f9897912f57055fe6d706f1d52e394109da86cc
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-20 21:32:56 -0400

    Initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
       CreatedBy = "Harbormaster"
       Blueprint = "Django"
       DomainModel = "Banking Industry Domain Model"
-      CertificationId = "ff2ba85c-71b6-4368-973d-67a1750b3130"
+      CertificationId = "c087cf19-0227-4a49-862a-3b2013f66266"
     }
   }
 }
@@ -39,7 +39,7 @@ resource "local_file" "private_key_pem" {
 }
 
 resource "aws_key_pair" "generated" {
-  key_name   = "pjsk-sshtest-0.7547459187401291"
+  key_name   = "pjsk-sshtest-0.30221763105450383"
   public_key = tls_private_key.generated.public_key_openssh
 
   lifecycle {
